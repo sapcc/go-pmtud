37221a6393b33213cee479718d40e5409eb6e209
Rene Kschamer <rene.kschamer@sap.com>
2026-08-17 14:07:00 +0200

    feat(e2e): wire lifecycle in Ginkgo BeforeSuite/AfterSuite

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -6,13 +6,33 @@
 package e2e
 
 import (
+	"os"
 	"testing"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
+	"github.com/sapcc/go-pmtud/lab"
 )
+
+var testLab *lab.Lab
 
 func TestE2E(t *testing.T) {
 	gomega.RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, "go-pmtud e2e")
 }
+
+var _ = ginkgo.BeforeSuite(func(ctx ginkgo.SpecContext) {
+	var err error
+	if os.Getenv("LAB_REUSE") != "" {
+		testLab, err = lab.Attach(ctx)
+	} else {
+		testLab, err = lab.Provision(ctx)
+	}
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+})
+
+var _ = ginkgo.AfterSuite(func(ctx ginkgo.SpecContext) {
+	if testLab != nil {
+		gomega.Expect(testLab.Teardown(ctx)).To(gomega.Succeed())
+	}
+})
